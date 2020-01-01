14a395b0e01fffaa6d4779cb1d465b6b91d61eda
Kaustubh <kaustubh.deshmukh27@gmail.com>
2020-01-01 11:32:49 +0530

    Add empty SplashActivity Add empty RegisterActivity

---
+++ app/src/main/java/com/dekaustubh/bingo/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.dekaustubh.bingo.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dekaustubh.bingo.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
